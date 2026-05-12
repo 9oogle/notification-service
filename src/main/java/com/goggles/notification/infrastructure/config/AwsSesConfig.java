f6a3c7a6673cb3b125209355cf4bb71e6662ab76
hyew0nn <hyewon-0607@naver.com>
2026-05-13 03:20:32 +0900

    feat: AWS SES 설정 및 요청 객체 구현

---
+++ src/main/java/com/goggles/notification/infrastructure/config/AwsSesConfig.java
@@ -0,0 +1,30 @@
+package com.goggles.notification.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sesv2.SesV2Client;
+
+@Configuration
+public class AwsSesConfig {
+
+  @Value("${aws.ses.access-key}")
+  private String accessKey;
+  @Value("${aws.ses.secret-key}")
+  private String secretKey;
+  @Value("${aws.region}")
+  private String region;
+
+  @Bean
+  public SesV2Client amazonSimpleEmailService() {
+    AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+    return SesV2Client.builder()
+        .region(Region.of(region))
+        .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+        .build();
+  }
+}
