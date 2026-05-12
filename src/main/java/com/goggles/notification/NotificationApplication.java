4d6da30c30d042d6c449d4e8417bc472f88e5be2
hyew0nn <hyewon-0607@naver.com>
2026-05-12 11:22:37 +0900

    feat: kafka 관련 설정 추가

--- src/main/java/com/goggles/notification/NotificationApplication.java
+++ src/main/java/com/goggles/notification/NotificationApplication.java
@@ -2,8 +2,12 @@ package com.goggles.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.goggles.notification", "com.goggles.common"})
+@EnableJpaRepositories(basePackages = {"com.goggles.notification", "com.goggles.common"})
 public class NotificationApplication {
 
   public static void main(String[] args) {
