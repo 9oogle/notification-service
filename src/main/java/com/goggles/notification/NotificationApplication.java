87389a841c37a1e3bd1ebd83d187e12c8dd4579e
hyew0nn <hyewon-0607@naver.com>
2026-05-13 03:18:42 +0900

    feat: AuditorAware 설정 파일 추가

--- src/main/java/com/goggles/notification/NotificationApplication.java
+++ src/main/java/com/goggles/notification/NotificationApplication.java
@@ -3,9 +3,11 @@ package com.goggles.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @EntityScan(basePackages = {"com.goggles.notification", "com.goggles.common"})
 @EnableJpaRepositories(basePackages = {"com.goggles.notification", "com.goggles.common"})
 public class NotificationApplication {
