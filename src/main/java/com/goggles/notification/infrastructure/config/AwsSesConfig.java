263601715284fe46f11f0d4db3afc3163f4e39e5
hyew0nn <hyewon-0607@naver.com>
2026-05-18 01:10:32 +0900

    test: gatling 설정 및 시물레이션 추가

--- src/main/java/com/goggles/notification/infrastructure/config/AwsSesConfig.java
+++ src/main/java/com/goggles/notification/infrastructure/config/AwsSesConfig.java
@@ -1,12 +1,20 @@
 package com.goggles.notification.infrastructure.config;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sesv2.SesV2Client;
+import software.amazon.awssdk.services.sesv2.model.SendEmailRequest;
+import software.amazon.awssdk.services.sesv2.model.SendEmailResponse;
 
 @Configuration
 public class AwsSesConfig {
@@ -21,12 +29,23 @@ public class AwsSesConfig {
   private String region;
 
   @Bean
+  @Profile("!load-test")
   public SesV2Client amazonSimpleEmailService() {
     AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
 
     return SesV2Client.builder()
         .region(Region.of(region))
         .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+        .endpointOverride(URI.create("http://localhost:4566"))
         .build();
+  }
+
+  @Bean
+  @Profile("load-test")
+  public SesV2Client mockSesV2Client() {
+    SesV2Client mock = Mockito.mock(SesV2Client.class);
+    when(mock.sendEmail(any(SendEmailRequest.class)))
+        .thenReturn(SendEmailResponse.builder().messageId("mock-message-id").build());
+    return mock;
   }
 }
